269b1ae9c2392a9c1edaf21de6dac026ab8f56f6
ows3090 <ows3090@naver.com>
2022-07-31 20:19:44 +0900

    Coroutine 기본 개념 학습 : 코루틴 빌더, 스코프, 컨텍스트, Suspend Function

---
+++ src/main/kotlin/coroutine/CoroutineThreadSleep.kt
@@ -0,0 +1,15 @@
+package coroutine
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    launch {
+        println("launch: ${Thread.currentThread().name}")
+        println("World!")
+    }
+
+    println("runBlocking: ${Thread.currentThread().name}")
+    Thread.sleep(1000L)
+    println("Hello")
+}
