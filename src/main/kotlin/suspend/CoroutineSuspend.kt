f248ea3021add9274186364c6aff8b8fdd964675
ows3090 <ows3090@naver.com>
2022-08-03 01:48:34 +0900

    코루틴 기본 개념 학습 : Async 사용

---
+++ src/main/kotlin/suspend/CoroutineSuspend.kt
@@ -0,0 +1,26 @@
+package suspend
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.random.Random
+import kotlin.system.measureTimeMillis
+
+suspend fun getRandom1(): Int{
+    delay(1000L)
+    return Random.nextInt(0, 500)
+}
+
+suspend fun getRandom2(): Int{
+    delay(1000L)
+    return Random.nextInt(0, 500)
+}
+
+fun main() = runBlocking {
+    val elapsedTime = measureTimeMillis {
+        val value1 = getRandom1()
+        val value2 = getRandom2()
+        println("${value1} + ${value2} = ${value1 + value2}")
+    }
+
+    println(elapsedTime)
+}
